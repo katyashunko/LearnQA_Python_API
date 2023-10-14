179a47cc6de9f4fa37006d29d87531c8fc3fad18
Kate <katya.shunko@gmail.com>
2023-10-14 23:40:04 +0200

    Task number 5(parsing)

--- main.py
+++ main.py
@@ -2,7 +2,5 @@ import json
 
 json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
 obj = json.loads(json_text)
-a = obj['messages']
-b = a[1]
-print(b['message'])
+print(obj['messages'][1]["message"])
 
