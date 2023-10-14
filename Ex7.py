889c54f307ff01a18d8f1bc74e36f771f9b763ab
Kate <katya.shunko@gmail.com>
2023-10-14 23:34:43 +0200

    Task number 7(method combinations)

--- Ex7.py
+++ Ex7.py
@@ -1,17 +1,17 @@
 import requests
 
 all_methods = [{"method":"GET"},{"method":"POST"},{"method":"PUT"},{"method":"DELETE"}]
+
 n = 0
 while n<= 3:
-    a = all_methods[n]
-    response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type",params=a)
-    print(f"GET + {a['method']} in params: " + response.text)
-    response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=a)
-    print(f"POST + {a['method']} in data: " + response.text)
-    response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=a)
-    print(f"PUT + {a['method']} in data: " + response.text)
-    response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=a)
-    print(f"DELETE + {a['method']} in data: " + response.text)
+    response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type",params=all_methods[n])
+    print(f"GET + {all_methods[n]['method']} in params: " + response.text)
+    response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=all_methods[n])
+    print(f"POST + {all_methods[n]['method']} in data: " + response.text)
+    response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=all_methods[n])
+    print(f"PUT + {all_methods[n]['method']} in data: " + response.text)
+    response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=all_methods[n])
+    print(f"DELETE + {all_methods[n]['method']} in data: " + response.text)
     n += 1
 
 
